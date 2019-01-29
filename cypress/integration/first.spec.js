f831cde1fc4c72e718a13c7538caf302e3ec8543
brettshollenberger <brett.shollenberger@gmail.com>
2019-01-29 13:45:25 -0500

    Add selector best practices

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -1,7 +1,20 @@
 context('Todos', () => {
-    it('visits our page', () => {
+    it('properly fills in seeds, and displays them appropriately', () => {
         cy.seed([{'text': "Hello World"}, {'text': 'Goodnight Moon', completed: true}])
-        cy.task('hello', {greeting: 'hi', name: 'world'})
         cy.visit("/")
+
+        // cy.get(':nth-child(1) > .view > label').should('have.text', 'Hello World')
+        cy.get('[data-cy=todo-1]')
+            .should('have.text', "Hello World")
+            .should('not.have.class', 'completed')
+            .find('.toggle')
+            .should('not.be.checked')
+
+        cy.get('[data-cy=todo-2]')
+            .should('have.text', "Goodnight Moon")
+            .should('have.class', 'completed')
+            .find('.toggle')
+            .should('be.checked')
+
     })
 })
